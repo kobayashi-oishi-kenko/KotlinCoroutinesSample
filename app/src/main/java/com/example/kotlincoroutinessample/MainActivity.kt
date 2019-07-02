3c557827e0c80f22b19cad7e56fb2b1aeecf499c
kobayashi-oishi-kenko <kobayashi@oishi-kenko.com>
2019-07-02 14:56:15 +0900

    add click listener and observe

--- app/src/main/java/com/example/kotlincoroutinessample/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutinessample/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.kotlincoroutinessample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        val rootLayout: ConstraintLayout = findViewById(R.id.roorView)
+        rootLayout.setOnClickListener {
+            viewModel.onMainViewClicked()
+        }
+
+        viewModel.snackbar.observe(this, Observer { text ->
+            text?.let {
+                Snackbar.make(rootLayout, text, Snackbar.LENGTH_SHORT).show()
+                viewModel.onSnackbarShown()
+            }
+        })
     }
 }
